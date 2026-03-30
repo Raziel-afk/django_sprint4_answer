296d5f5f3d4f7393426e413d3288f1c1492bd420
Raziel-afk <tonic1101@mail.ru>
2026-03-30 22:27:08 +0300

    Update forms.py

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -3,6 +3,18 @@ from django import forms
 from .models import Comment
 
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'pub_date', 'location', 'category', 'image', 'is_published']
+        widgets = {
+            'pub_date': forms.DateTimeInput(
+                format='%Y-%m-%dT%H:%M',
+                attrs={'type': 'datetime-local'}
+            ),
+        }
+
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
